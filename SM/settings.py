9f9b1dee69317db5071e9463d3b7bb0843c75ae5
smeer007 <sameeruikey16@gmail.com>
2022-03-28 17:03:04 +0530

    settings

--- SM/settings.py
+++ SM/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
